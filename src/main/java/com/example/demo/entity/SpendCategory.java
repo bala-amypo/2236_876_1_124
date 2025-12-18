a84cc1d256ad4cf22f7685e0b1c51e97a871af40
bala-amypo <bala@amypo.in>
2025-12-18 14:28:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/SpendCategory.java
@@ -0,0 +1,30 @@
+[ERROR] COMPILATION ERROR : 
+[INFO] -------------------------------------------------------------
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/entity/DiversityClassification.java:[26,17] cannot find symbol
+  symbol:   class DiversityTarget
+  location: class com.example.demo.entity.DiversityClassification
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/entity/Supplier.java:[45,17] cannot find symbol
+  symbol:   class PurchaseOrder
+  location: class com.example.demo.entity.Supplier
+[INFO] 2 errors 
+[INFO] -------------------------------------------------------------
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  10.095 s
+[INFO] Finished at: 2025-12-18T14:21:50Z
+[INFO] ------------------------------------------------------------------------
+[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project demo: Compilation failure: Compilation failure: 
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/entity/DiversityClassification.java:[26,17] cannot find symbol
+[ERROR]   symbol:   class DiversityTarget
+[ERROR]   location: class com.example.demo.entity.DiversityClassification
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/entity/Supplier.java:[45,17] cannot find symbol
+[ERROR]   symbol:   class PurchaseOrder
+[ERROR]   location: class com.example.demo.entity.Supplier
+[ERROR] -> [Help 1]
+[ERROR] 
+[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
+[ERROR] Re-run Maven using the -X switch to enable full debug logging.
+[ERROR] 
+[ERROR] For more information about the errors and possible solutions, please read the following articles:
+[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
