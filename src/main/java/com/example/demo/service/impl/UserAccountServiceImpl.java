66edbaeb62446281489156cdf325ba291d65d516
bala-amypo <bala@amypo.in>
2025-12-18 15:49:38 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.UserAccount;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.UnauthorizedException;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.service.UserAccountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService {
+
+    private final UserAccountRepository repository;
+
+    public UserAccountServiceImpl(UserAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserAccount register(UserAccount user) {
+        return repository.save(user);
+    }
+
+    @Override
+    public UserAccount login(String email, String password) {
+        UserAccount user = repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found"));
+
+        if (!user.getPassword().equals(password)) {
+            throw new UnauthorizedException("Invalid credentials");
+        }
+
+        return user;
+    }
+}
