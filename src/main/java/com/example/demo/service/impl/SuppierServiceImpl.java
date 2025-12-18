ce5a85c9ddc4e6b9e7f21154201e5cedf3a203a8
bala-amypo <bala@amypo.in>
2025-12-18 13:58:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SuppierServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Supplier;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.SupplierRepository;
+import com.example.demo.service.SupplierService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+
+    private final SupplierRepository supplierRepository;
+
+    public SupplierServiceImpl(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    @Override
+    public Supplier createSupplier(Supplier supplier) {
+        if (supplierRepository.existsByEmail(supplier.getEmail())) {
+            throw new BadRequestException("Email already exists");
+        }
+        return supplierRepository.save(supplier);
+    }
+
+    @Override
+    public Supplier getSupplierById(Long id) {
+        return supplierRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Supplier not found"));
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    @Override
+    public void deactivateSupplier(Long id) {
+        Supplier supplier = getSupplierById(id);
+        supplier.setIsActive(false);
+        supplierRepository.save(supplier);
+    }
+}
