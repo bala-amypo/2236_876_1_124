c0fd804cead950c9c395cd031790d108611d621c
bala-amypo <bala@amypo.in>
2025-12-18 08:42:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/DiversityClassification.java
+++ src/main/java/com/example/demo/entity/DiversityClassification.java
@@ -10,7 +10,7 @@ import jakarta.persistence.*;
 public class DiversityClassification {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
--- src/main/java/com/example/demo/entity/Supplier.java
+++ src/main/java/com/example/demo/entity/Supplier.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Supplier {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   
     private Long id;
 
     @Column(nullable = false)
