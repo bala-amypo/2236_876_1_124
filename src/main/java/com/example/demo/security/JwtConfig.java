07a5eafcfa08495f4593586da2abc466de3cc893
bala-amypo <bala@amypo.in>
2025-12-18 18:28:43 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtConfig.java
+++ src/main/java/com/example/demo/security/JwtConfig.java
@@ -8,10 +8,6 @@ public class JwtConfig {
 
     @Bean
     public JwtUtil jwtUtil() {
-        // secret must be at least 32 bytes
-        byte[] secret = "my-secret-key-my-secret-key-123".getBytes();
-        Long expirationMs = 86400000L; // 1 day
-
-        return new JwtUtil(secret, expirationMs);
+        return new JwtUtil();
     }
 }
