f82fe1a33ce6a01973e5412f494d38e90b0a443c
bala-amypo <bala@amypo.in>
2025-12-18 14:07:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/DiversityClassificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.DiversityClassification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiversityClassificationRepository
-        extends JpaRepository<DiversityClassification, Long> {
-}
--- src/main/java/com/example/demo/repository/SpendCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.SpendCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpendCategoryRepository extends JpaRepository<SpendCategory, Long> {
-}
