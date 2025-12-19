a5451cf21838e90e8e42a89bf8d7192e0b73dee4
bala-amypo <bala@amypo.in>
2025-12-19 04:40:57 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserAccountRepository
-        extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
     Optional<UserAccount> findByEmail(String email);
+
+    boolean existsByEmail(String email); // ✅ This fixes your compilation errors
 }
