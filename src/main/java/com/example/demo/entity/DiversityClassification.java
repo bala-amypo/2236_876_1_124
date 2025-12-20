5240a1592109f9c7a768e4bbf42b46599f28ccba
bala-amypo <bala@amypo.in>
2025-12-20 05:29:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/DiversityClassification.java
+++ src/main/java/com/example/demo/entity/DiversityClassification.java
@@ -27,7 +27,6 @@ public class DiversityClassification {
     }
 
     // ---------------- Getters & Setters ----------------
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
