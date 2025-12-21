dc814d759fe5212d86653119189508f29fa85ed0
bala-amypo <bala@amypo.in>
2025-12-21 12:08:35 +0000

    User commit

--- src/main/java/com/example/demo/service/DiversityTargetService.java
+++ src/main/java/com/example/demo/service/DiversityTargetService.java
@@ -4,9 +4,8 @@ import com.example.demo.entity.DiversityTarget;
 import java.util.List;
 
 public interface DiversityTargetService {
-    DiversityTarget create(DiversityTarget entity);
-    List<DiversityTarget> getAll();
-    DiversityTarget getById(Long id);
-    DiversityTarget update(Long id, DiversityTarget entity);
-    void delete(Long id);
+    DiversityTarget createTarget(DiversityTarget target);
+    List<DiversityTarget> getTargetsByYear(int year);
+    List<DiversityTarget> getAllTargets();
+    void deactivateTarget(Long id);
 }
