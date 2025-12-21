5032acc97f61b76168b96b8aa114aed77e7453dc
bala-amypo <bala@amypo.in>
2025-12-21 12:04:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/DiversityClassificationRepository.java
+++ src/main/java/com/example/demo/repository/DiversityClassificationRepository.java
@@ -2,8 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DiversityClassification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
-@Repository
 public interface DiversityClassificationRepository extends JpaRepository<DiversityClassification, Long> {
+    List<DiversityClassification> findByActiveTrue();
 }
