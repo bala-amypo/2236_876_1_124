16280b8383c3561f3cb73d00dc14df87f5357023
bala-amypo <bala@amypo.in>
2025-12-21 12:04:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/SpendCategoryRepository.java
+++ src/main/java/com/example/demo/repository/SpendCategoryRepository.java
@@ -2,13 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.SpendCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface SpendCategoryRepository extends JpaRepository<SpendCategory, Long> {
-
-    // Custom query for active categories
     List<SpendCategory> findByActiveTrue();
 }
