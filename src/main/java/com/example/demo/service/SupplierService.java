a05102b4efbc6809ed0cea356673459da75e23d6
bala-amypo <bala@amypo.in>
2025-12-21 12:07:00 +0000

    User commit

--- src/main/java/com/example/demo/service/SupplierService.java
+++ src/main/java/com/example/demo/service/SupplierService.java
@@ -1,16 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Supplier;
-
 import java.util.List;
 
 public interface SupplierService {
-
     Supplier createSupplier(Supplier supplier);
-
     Supplier getSupplierById(Long id);
-
     List<Supplier> getAllSuppliers();
-
     void deactivateSupplier(Long id);
 }
